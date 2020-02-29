0688cdb8f31f42fb1113713a1914d62235b4286c
LudovicSchorpp <emmalu@hotmail.ch>
2020-02-29 15:17:12 +0100

    update CHABART

--- codes_tests/Chabart/notebooks/Chabart_fun.py
+++ codes_tests/Chabart/notebooks/Chabart_fun.py
@@ -11,7 +11,7 @@ def pump_data(file,nrow=38,ncol=42,layer=0,fac=1):
     This function take a file (csv) path as entry and spatial parameters (nrow,ncol) and the layer on which the data will be applied
     actually import data from the file in a df, remove value above 3000 and return in a list all the values different from 0 with the appropriate cellid (dis only)
     structure : lst = pump_data(file,nrow,ncol,layer,fac)
-    a factor is added in order to change the units of the pumping values, the factor divide the actual values in the file
+    a factor is added in order to change the units of the pumping values, the factor multiply the actual values in the file
     """
     
     lst=[]
@@ -21,6 +21,6 @@ def pump_data(file,nrow=38,ncol=42,layer=0,fac=1):
     for irow in np.arange(nrow):
         for icol in np.arange(ncol):
             if a.iloc[irow,icol] != 0:
-                data_pompage = ((layer,irow,icol),a.iloc[irow,icol]/fac)
+                data_pompage = ((layer,irow,icol),a.iloc[irow,icol]*fac)
                 lst.append(data_pompage)
     return lst
