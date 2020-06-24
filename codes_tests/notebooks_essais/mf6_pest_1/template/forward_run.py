7c68507882767afe00ed6b393c04c85b098a7ebd
LudovicSchorpp <51512737+LudovicSchorpp@users.noreply.github.com>
2020-06-24 13:49:59 +0200

    ça marche toujours pas :(

--- codes_tests/notebooks_essais/mf6_pest_1/template/forward_run.py
+++ codes_tests/notebooks_essais/mf6_pest_1/template/forward_run.py
@@ -10,7 +10,7 @@ def main():
     except Exception as e:
        print('error removing tmp file:.\heads.csv')
     pyemu.helpers.apply_list_and_array_pars(arr_par_file='mult2model_info.csv')
-    pyemu.os_utils.run(r'mf6')
+    pyemu.os_utils.run(r'..\mf6')
 
 
 if __name__ == '__main__':
